9758c92de71a3538468eaff2642063107d027052
Samir Comprés <samirant15@gmail.com>
2022-04-18 09:18:11 -0400

    BooleanToggle3State n/a changes

--- components/BooleanToggle3State.js
+++ components/BooleanToggle3State.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { StyleSheet } from 'react-native';
 import { Button, ButtonGroup, Text } from '@ui-kitten/components';
 
-const BooleanToggle3State = ({ value, controlled, onChange, positiveTxt, naText, negativeTxt, disabled, colors }) => {
+const BooleanToggle3State = ({ value, controlled, onChange, positiveTxt, onlyNa, hideNa, naText, negativeTxt, disabled, colors }) => {
 
     const { positiveTxtColorFg, positiveTxtColorBg } = colors || { positiveTxtColorFg: null, positiveTxtColorBg: null };
     const { negativeTxtColorFg, negativeTxtColorBg } = colors || { negativeTxtColorFg: null, negativeTxtColorBg: null };
@@ -27,27 +27,27 @@ const BooleanToggle3State = ({ value, controlled, onChange, positiveTxt, naText,
     return (
         val === 'true' ? (
             <ButtonGroup size='small' appearance='outline' status='control'>
-                <Button onPress={() => changeValue('true')} style={{ backgroundColor: positiveTxtColorBg || '#90E0A6' }}><Text style={{ ...styles.bold, color: positiveTxtColorFg || '#1B5F2E' }}>{positiveTxt || 'Sí'}</Text></Button>
-                <Button onPress={() => changeValue('false')} style={{ backgroundColor: neutralTxtColorBg || '#EAEAEC' }}><Text style={{ ...styles.bold, color: neutralTxtColorFg || '#000000' }}>{negativeTxt || 'No'}</Text></Button>
-                <Button onPress={() => changeValue('na')} style={{ backgroundColor: neutralTxtColorBg || '#EAEAEC' }}><Text style={{ ...styles.bold, color: neutralTxtColorFg || '#000000' }}>{naText || 'N/A'}</Text></Button>
+                <Button onPress={() => changeValue('true')} style={{ display: onlyNa ? 'none' : null, backgroundColor: positiveTxtColorBg || '#90E0A6' }}><Text style={{ ...styles.bold, color: positiveTxtColorFg || '#1B5F2E' }}>{positiveTxt || 'Sí'}</Text></Button>
+                <Button onPress={() => changeValue('false')} style={{ display: onlyNa ? 'none' : null, backgroundColor: neutralTxtColorBg || '#EAEAEC' }}><Text style={{ ...styles.bold, color: neutralTxtColorFg || '#000000' }}>{negativeTxt || 'No'}</Text></Button>
+                <Button onPress={() => changeValue('na')} style={{ display: hideNa ? 'none' : null, backgroundColor: neutralTxtColorBg || '#EAEAEC' }}><Text style={{ ...styles.bold, color: neutralTxtColorFg || '#000000' }}>{naText || 'N/A'}</Text></Button>
             </ButtonGroup>
         ) : val === 'false' ? (
             <ButtonGroup size='small' appearance='outline' status='control'>
-                <Button onPress={() => changeValue('true')} style={{ backgroundColor: neutralTxtColorBg || '#EAEAEC' }}><Text style={{ ...styles.bold, color: neutralTxtColorFg || '#000000' }}>{positiveTxt || 'Sí'}</Text></Button>
-                <Button onPress={() => changeValue('false')} style={{ backgroundColor: negativeTxtColorBg || '#FF8585' }}><Text style={{ ...styles.bold, color: negativeTxtColorFg || '#B80000' }}>{negativeTxt || 'No'}</Text></Button>
-                <Button onPress={() => changeValue('na')} style={{ backgroundColor: neutralTxtColorBg || '#EAEAEC' }}><Text style={{ ...styles.bold, color: neutralTxtColorFg || '#000000' }}>{naText || 'N/A'}</Text></Button>
+                <Button onPress={() => changeValue('true')} style={{ display: onlyNa ? 'none' : null, backgroundColor: neutralTxtColorBg || '#EAEAEC' }}><Text style={{ ...styles.bold, color: neutralTxtColorFg || '#000000' }}>{positiveTxt || 'Sí'}</Text></Button>
+                <Button onPress={() => changeValue('false')} style={{ display: onlyNa ? 'none' : null, backgroundColor: negativeTxtColorBg || '#FF8585' }}><Text style={{ ...styles.bold, color: negativeTxtColorFg || '#B80000' }}>{negativeTxt || 'No'}</Text></Button>
+                <Button onPress={() => changeValue('na')} style={{ display: hideNa ? 'none' : null, backgroundColor: neutralTxtColorBg || '#EAEAEC' }}><Text style={{ ...styles.bold, color: neutralTxtColorFg || '#000000' }}>{naText || 'N/A'}</Text></Button>
             </ButtonGroup>
         ) : val === 'na' ? (
             <ButtonGroup size='small' appearance='outline' status='control'>
-                <Button onPress={() => changeValue('true')} style={{ backgroundColor: neutralTxtColorBg || '#EAEAEC' }}><Text style={{ ...styles.bold, color: neutralTxtColorFg || '#000000' }}>{positiveTxt || 'Sí'}</Text></Button>
-                <Button onPress={() => changeValue('false')} style={{ backgroundColor: neutralTxtColorBg || '#EAEAEC' }}><Text style={{ ...styles.bold, color: neutralTxtColorFg || '#000000' }}>{naText || 'No'}</Text></Button>
-                <Button onPress={() => changeValue('na')} style={{ backgroundColor: naTxtColorBg || '#F5B7BC' }}><Text style={{ ...styles.bold, color: naTxtColorFg || '#331B1D' }}>{naText || 'N/A'}</Text></Button>
+                <Button onPress={() => changeValue('true')} style={{ display: onlyNa ? 'none' : null, backgroundColor: neutralTxtColorBg || '#EAEAEC' }}><Text style={{ ...styles.bold, color: neutralTxtColorFg || '#000000' }}>{positiveTxt || 'Sí'}</Text></Button>
+                <Button onPress={() => changeValue('false')} style={{ display: onlyNa ? 'none' : null, backgroundColor: neutralTxtColorBg || '#EAEAEC' }}><Text style={{ ...styles.bold, color: neutralTxtColorFg || '#000000' }}>{naText || 'No'}</Text></Button>
+                <Button onPress={() => changeValue('na')} style={{ display: hideNa ? 'none' : null, backgroundColor: naTxtColorBg || '#F5B7BC' }}><Text style={{ ...styles.bold, color: naTxtColorFg || '#331B1D' }}>{naText || 'N/A'}</Text></Button>
             </ButtonGroup>
         ) : (
             <ButtonGroup size='small' appearance='outline' status='control'>
-                <Button onPress={() => changeValue('true')} style={{ backgroundColor: neutralTxtColorBg || '#EAEAEC' }}><Text style={{ ...styles.bold, color: neutralTxtColorFg || '#000000' }}>{positiveTxt || 'Sí'}</Text></Button>
-                <Button onPress={() => changeValue('false')} style={{ backgroundColor: neutralTxtColorBg || '#EAEAEC' }}><Text style={{ ...styles.bold, color: neutralTxtColorFg || '#000000' }}>{negativeTxt || 'No'}</Text></Button>
-                <Button onPress={() => changeValue('na')} style={{ backgroundColor: neutralTxtColorBg || '#EAEAEC' }}><Text style={{ ...styles.bold, color: neutralTxtColorFg || '#000000' }}>{naText || 'N/A'}</Text></Button>
+                <Button onPress={() => changeValue('true')} style={{ display: onlyNa ? 'none' : null, backgroundColor: neutralTxtColorBg || '#EAEAEC' }}><Text style={{ ...styles.bold, color: neutralTxtColorFg || '#000000' }}>{positiveTxt || 'Sí'}</Text></Button>
+                <Button onPress={() => changeValue('false')} style={{ display: onlyNa ? 'none' : null, backgroundColor: neutralTxtColorBg || '#EAEAEC' }}><Text style={{ ...styles.bold, color: neutralTxtColorFg || '#000000' }}>{negativeTxt || 'No'}</Text></Button>
+                <Button onPress={() => changeValue('na')} style={{ display: hideNa ? 'none' : null, backgroundColor: neutralTxtColorBg || '#EAEAEC' }}><Text style={{ ...styles.bold, color: neutralTxtColorFg || '#000000' }}>{naText || 'N/A'}</Text></Button>
             </ButtonGroup>
         )
     )
